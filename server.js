65454ea7c465c6bdc05396b536d6f03990eea8b0
Jonas Augusto <jonasaugusto99@gmail.com>
2022-09-06 16:42:52 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+
+app.get("/api/quotes/random", (req, res, next) => {
+    const quote = getRandomElement(quotes);
+    res.status(200).send({ quote: quote });
+  });
+
+app.get('/api/quotes', (req, res) => {
+    if(req.query.person){
+        const author = req.query.person
+        const result = quotes.filter(quote => quote.person === author)
+
+        if(result.length > 0){
+            res.status(200).send({quotes: result})
+        } else {
+            res.status(404).send()
+        }
+        
+    } else {
+        res.status(200).send({quotes: quotes})
+    }
+    
+})
+
+app.post('/api/quotes', (req, res) => {
+
+    if(req.query.quote && req.query.person){
+        const newQuote = {
+            quote: req.query.quote,
+            person: req.query.person
+        }
+        quotes.push(newQuote)
+        res.status(201).send({quote: newQuote})
+    } else {
+        res.status(400)
+    }
+    
+})
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT)
+})
+
